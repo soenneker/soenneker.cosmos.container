79a0f87f2cd040cb5049ce030deeacaa370c624f
Jake Soenneker <jake@soenneker.com>
2023-04-09 11:32:00 -0500

    registrar, deps

--- src/Registrars/CosmosContainerUtilRegistrar.cs
+++ src/Registrars/CosmosContainerUtilRegistrar.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Soenneker.Cosmos.Client.Registrars;
 using Soenneker.Cosmos.Container.Abstract;
+using Soenneker.Cosmos.Container.Setup.Registrars;
 
 namespace Soenneker.Cosmos.Container.Registrars;
 
@@ -14,6 +16,8 @@ public static class CosmosContainerUtilRegistrar
     /// </summary>
     public static void AddCosmosContainerUtil(this IServiceCollection services)
     {
+        services.AddCosmosClientUtil();
+        services.AddCosmosContainerSetupUtil();
         services.TryAddSingleton<ICosmosContainerUtil, CosmosContainerUtil>();
     }
 }
